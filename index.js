a30d1c07a6fcb405514f1d17c488a65aa30ffebd
Eevi Luukkonen <eevi.luukkonen@gmail.com>
2022-10-27 12:25:50 +0300

    uibuild

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const morgan = require('morgan')
 const cors = require('cors')
 const app = express()
 const Person = require('./models/person')
+const { default: mongoose } = require('mongoose')
 
 app.use(express.static('build'))
 app.use(express.json())
@@ -53,7 +54,7 @@ app.delete('/api/persons/:id', (req, res, next) => {
       .catch(error => next(error))
 })
 
-app.post('/api/persons', (req, res) => {    
+app.post('/api/persons', (req, res, next) => {    
     const body = req.body
 
     if (!body.name) {
@@ -73,9 +74,11 @@ app.post('/api/persons', (req, res) => {
       number: body.number,
     })
 
-    person.save().then(savedPerson => {
-      res.json(savedPerson)
-    })
+    person.save()
+      .then(savedPerson => {
+        res.json(savedPerson)
+      })
+      .catch(error => next(error))
 
 })
 
@@ -102,6 +105,8 @@ const errorHandler = (error, req, res, next) => {
 
   if (error.name === 'CastError') {
     return res.status(400).send({ error: 'malformatted id' })
+  } else if (error.name == 'ValidationError') {
+    return res.status(400).json({ error: error.message })
   }
 
   next(error)
